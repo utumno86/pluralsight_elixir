de8bab2df32180328c80b6ae8c4e4d7baa036b98
Michael Nash <michael.nash@kin.com>
2023-09-22 16:40:30 -0400

    require

--- module_playground.exs
+++ module_playground.exs
@@ -1,6 +1,7 @@
 defmodule ModulePlayground do
   import IO, only: [puts: 1]
   import Kernel, except: [inspect: 1]
+  require Integer
   alias ModulePlayground.Misc.Util.Math
 
   def say_here do
@@ -15,5 +16,9 @@ defmodule ModulePlayground do
 
   def print_sum do
     Math.add(1,2)
+  end
+
+  def print_is_even(num) do
+    puts "Is #{num} even? #{Integer.is_even(num)}"
   end
 end
