e75894d5a383f805aab3e50778e2edb69ba653b2
Michael Nash <michael.nash@kin.com>
2023-09-22 16:26:36 -0400

    except import

--- module_playground.exs
+++ module_playground.exs
@@ -1,7 +1,14 @@
 defmodule ModulePlayground do
   import IO, only: [puts: 1]
+  import Kernel, except: [inspect: 1]
 
   def say_here do
     puts "I'm Here"
+  end
+
+  def inspect(param1) do
+    puts "starting output"
+    puts param1
+    puts "ending output"
   end
 end
