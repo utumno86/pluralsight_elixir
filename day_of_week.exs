197882739902b8b7704809437336c83250fab11c
Michael Nash <michael.nash@kin.com>
2023-10-27 16:01:20 -0400

    practicing cond

---
+++ day_of_week.exs
@@ -0,0 +1,13 @@
+defmodule Sample.Calendar do
+  def day_abbreviation(day) do
+    cond do
+      day == :Monday -> "M"
+      day == :Tuesday -> "M"
+      day == :Wednesday -> "W"
+      day == :Thursday -> "Th"
+      day == :Friday -> "F"
+      day == :Saturday -> "Sa"
+      day == :Sunday -> "Su"
+    end
+  end
+end
