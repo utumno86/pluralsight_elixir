ba71f66fe8ee887226cddc7068b2e3101fe1101e
Michael Nash <michael.nash@kin.com>
2023-10-27 15:15:51 -0400

    private functions

--- sample_enum.exs
+++ sample_enum.exs
@@ -1,6 +1,16 @@
 defmodule Sample.Enum do
 
   def first([]), do: nil
-
   def first([head | _]), do: head
+  def first([], val), do: val
+
+
+  def add(list, val \\ 0) do
+    trace(val)
+    [val | list]
+  end
+
+  defp trace(string) do
+    IO.puts("The value passed in was #{string}")
+  end
 end
