cb260ad14ff77e2c8768188044734e72abfa50b4
Michael Nash <michael.nash@kin.com>
2023-10-27 16:14:32 -0400

    error handling

--- day_of_week.exs
+++ day_of_week.exs
@@ -19,6 +19,7 @@ defmodule Sample.Calendar do
       {25, month, _} -> "Only #{12 - month} months until Christmas"
       {31, 10, 1517} -> "The reformation is starting!"
       {31,10, _} -> "Happy Halloween"
+      {_ , month , _} when month <= 12 -> "Just an average day"
       {_ , _ , _} -> "Just an average day"
     end
   end
